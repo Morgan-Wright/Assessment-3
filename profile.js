a27e779b9f688f57a4386cb77c3d012a24380794
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-03-26 19:04:24 -0700

    assessment complete

---
+++ profile.js
@@ -0,0 +1,17 @@
+let buttonOne = document.getElementById("animal")
+let buttonTwo = document.getElementById("food")
+let buttonThree = document.getElementById("nickname")
+
+
+
+buttonOne.addEventListener('click', () =>{
+	alert('chicken')
+})
+
+buttonTwo.addEventListener('click', () =>{
+	alert('chicken')
+})
+
+buttonThree.addEventListener('click', () =>{
+	alert('chicken')
+})
